61f8bdf455142ee63c19953329f3bcf5bfffe842
Nikhil0911 <35520876+Nikhil0911@users.noreply.github.com>
2023-07-04 17:39:30 +0530

    Update Show_PPT_in_Python_Tkinter_App.py
    Changed the whole code

--- Show_PPT_in_Python_Tkinter_App.py
+++ Show_PPT_in_Python_Tkinter_App.py
@@ -1,82 +1,71 @@
 import tkinter as tk
-from tkinter import ttk
-import win32com.client as win32
+from PIL import Image, ImageTk
+from pptx import Presentation
 
-class PowerPointViewer:
-    def __init__(self, root):
-        self.root = root
-        self.root.title("PowerPoint Viewer")
-        
-        self.ppt_file = None
-        self.slides = None
-        self.current_slide_index = 0
-        
-        self.create_widgets()
-    
-    def create_widgets(self):
-        # File selection frame
-        file_frame = ttk.Frame(self.root)
-        file_frame.pack(pady=10)
-        
-        file_label = ttk.Label(file_frame, text="PowerPoint File:")
-        file_label.pack(side="left")
-        
-        self.file_entry = ttk.Entry(file_frame, width=30)
-        self.file_entry.pack(side="left")
-        
-        file_button = ttk.Button(file_frame, text="Browse", command=self.open_ppt_file)
-        file_button.pack(side="left")
-        
-        # Slide navigation frame
-        nav_frame = ttk.Frame(self.root)
-        nav_frame.pack(pady=10)
-        
-        prev_button = ttk.Button(nav_frame, text="Previous", command=self.show_previous_slide)
-        prev_button.pack(side="left")
-        
-        next_button = ttk.Button(nav_frame, text="Next", command=self.show_next_slide)
-        next_button.pack(side="left")
-        
-        # Slide display frame
-        self.slide_frame = ttk.Frame(self.root)
-        self.slide_frame.pack(pady=10)
-        
-        self.slide_label = ttk.Label(self.slide_frame, text="")
-        self.slide_label.pack()
-    
-    def open_ppt_file(self):
-        self.ppt_file = tk.filedialog.askopenfilename(filetypes=[("PowerPoint Files", "*.pptx")])
-        self.file_entry.delete(0, tk.END)
-        self.file_entry.insert(0, self.ppt_file)
-        
-        if self.ppt_file:
-            self.open_ppt_slides()
-    
-    def open_ppt_slides(self):
-        ppt_app = win32.gencache.EnsureDispatch("PowerPoint.Application")
-        presentation = ppt_app.Presentations.Open(self.ppt_file)
-        self.slides = presentation.Slides
-    
-    def show_previous_slide(self):
-        if self.slides and self.current_slide_index > 0:
-            self.current_slide_index -= 1
-            self.show_slide()
-    
-    def show_next_slide(self):
-        if self.slides and self.current_slide_index < self.slides.Count - 1:
-            self.current_slide_index += 1
-            self.show_slide()
-    
+class PowerPointViewer(tk.Tk):
+    def __init__(self, presentation_file):
+        super().__init__()
+        self.presentation = Presentation(presentation_file)
+        self.current_slide = 0
+        self.total_slides = len(self.presentation.slides)
+
+        self.title("PowerPoint Viewer")
+        self.geometry("800x600")
+        self.configure(bg="white")
+
+        self.slide_image_label = tk.Label(self, bg="white")
+        self.slide_image_label.pack(expand=True, fill="both")
+
+        self.previous_button = tk.Button(self, text="Previous", command=self.show_previous_slide)
+        self.previous_button.pack(side="left", padx=20, pady=10)
+
+        self.next_button = tk.Button(self, text="Next", command=self.show_next_slide)
+        self.next_button.pack(side="right", padx=20, pady=10)
+
+        self.show_slide()
+
     def show_slide(self):
-        if self.slides:
-            slide = self.slides[self.current_slide_index + 1]
-            slide.Export("temp.png", "PNG")  # Export slide as an image
-            
-            # Load the exported image and display it in the frame
-            image = tk.PhotoImage(file="temp.png")
-            self.slide_label.configure(image=image)
-            self.slide_label.image = image
-    
-root = tk.Tk()
-app = PowerPointViewer(root)
-root.mainloop()
+        slide = self.presentation.slides[self.current_slide]
+        slide_image = self.get_slide_image(slide)
+        self.update_slide_image(slide_image)
+
+    def show_next_slide(self):
+        self.current_slide = (self.current_slide + 1) % self.total_slides
+        self.show_slide()
+
+    def show_previous_slide(self):
+        self.current_slide = (self.current_slide - 1) % self.total_slides
+        self.show_slide()
+
+    def get_slide_image(self, slide):
+        slide_width = 720
+        slide_height = 540
+        slide_image = Image.new("RGB", (slide_width, slide_height), color="white")
+
+        for shape in slide.shapes:
+            if shape.shape_type == 13:  # Shape type 13 represents a picture in python-pptx
+                image_bytes = shape.image.blob
+                image_stream = io.BytesIO(image_bytes)
+                image = Image.open(image_stream)
+                image = image.resize((slide_width, slide_height))
+                slide_image.paste(image, (0, 0))
+
+        return slide_image
+
+    def update_slide_image(self, slide_image):
+        slide_image_tk = ImageTk.PhotoImage(slide_image)
+        self.slide_image_label.config(image=slide_image_tk)
+        self.slide_image_label.image = slide_image_tk
+
+if __name__ == "__main__":
+    import io
+    import os
+
+    # Replace 'your_presentation_file.pptx' with the actual PowerPoint file path
+    presentation_file = "your_presentation_file.pptx"
+
+    if os.path.exists(presentation_file):
+        app = PowerPointViewer(presentation_file)
+        app.mainloop()
+    else:
+        print("Presentation file not found.")
