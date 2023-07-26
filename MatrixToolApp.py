4d59ec36ba1c6156b2b7d3762840c0fe4df2d6dc
Nikhil0911 <35520876+Nikhil0911@users.noreply.github.com>
2023-07-26 14:54:30 +0530

    Update MatrixToolApp.py
    New frame will be opened when clicked on run

--- MatrixToolApp.py
+++ MatrixToolApp.py
@@ -2,43 +2,70 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
-class MatrixToolApp(tk.Tk):
-    def __init__(self):
-        super().__init__()
-        self.title("Matrix Tool")
-        self.geometry("400x300")
+class InputFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Matrix Tool")
+        self.pack()
 
         self.file1 = None
         self.file2 = None
         self.folder = None
-        self.matrix_data = [[0, 0], [0, 0]]
 
         self.create_widgets()
 
     def create_widgets(self):
-        # File 1 input
         file1_label = tk.Label(self, text="Select File 1:")
         file1_label.pack()
         self.file1_btn = tk.Button(self, text="Choose File 1", command=self.select_file1)
         self.file1_btn.pack()
 
-        # File 2 input
         file2_label = tk.Label(self, text="Select File 2:")
         file2_label.pack()
         self.file2_btn = tk.Button(self, text="Choose File 2", command=self.select_file2)
         self.file2_btn.pack()
 
-        # Folder input
         folder_label = tk.Label(self, text="Select Folder:")
         folder_label.pack()
         self.folder_btn = tk.Button(self, text="Choose Folder", command=self.select_folder)
         self.folder_btn.pack()
 
-        # Run button
-        self.run_btn = tk.Button(self, text="Run", command=self.run_tool)
+        self.run_btn = tk.Button(self, text="Run", command=self.open_matrix_frame)
         self.run_btn.pack()
 
-        # Dropdowns
+    def select_file1(self):
+        self.file1 = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
+
+    def select_file2(self):
+        self.file2 = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
+
+    def select_folder(self):
+        self.folder = filedialog.askdirectory()
+
+    def open_matrix_frame(self):
+        if self.file1 and self.file2 and self.folder:
+            self.master.withdraw()  # Hide the current frame
+            matrix_window = tk.Toplevel(self.master)  # Create a new top-level window
+            matrix_frame = MatrixFrame(matrix_window, self.file1, self.file2, self.folder)
+        else:
+            tk.messagebox.showwarning("Input Error", "Please select File 1, File 2, and Folder first!")
+
+class MatrixFrame(tk.Frame):
+    def __init__(self, master, file1, file2, folder):
+        super().__init__(master)
+        self.master = master
+        self.master.title("Matrix Frame")
+        self.pack()
+
+        self.file1 = file1
+        self.file2 = file2
+        self.folder = folder
+        self.matrix_data = [[0, 0], [0, 0]]
+
+        self.create_widgets()
+
+    def create_widgets(self):
         self.dropdown1 = tk.StringVar(self)
         self.dropdown2 = tk.StringVar(self)
         self.dropdown3 = tk.StringVar(self)
@@ -61,24 +88,15 @@ class MatrixToolApp(tk.Tk):
         self.dropdown3_menu.pack()
         self.dropdown3_menu.config(state=tk.DISABLED)
 
-        # Matrix display
         self.matrix_label = tk.Label(self, text="Matrix:")
         self.matrix_label.pack()
         self.matrix_display = tk.Label(self, text="")
         self.matrix_display.pack()
 
-    def select_file1(self):
-        self.file1 = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
+        self.populate_dropdowns()
 
-    def select_file2(self):
-        self.file2 = filedialog.askopenfilename(filetypes=[("All Files", "*.*")])
-
-    def select_folder(self):
-        self.folder = filedialog.askdirectory()
-
-    def run_tool(self):
-        # Perform some operations based on the selected files and folder
-        # For this example, we'll just populate the dropdowns and matrix with sample data.
+    def populate_dropdowns(self):
+        # For this example, we'll populate the dropdowns with sample data.
         self.dropdown1_menu.config(state=tk.NORMAL)
         self.dropdown1_menu["menu"].delete(0, "end")
         self.dropdown1_menu["menu"].add_command(label="Option 1", command=lambda: self.update_matrix(1, 1))
@@ -102,5 +120,6 @@ class MatrixToolApp(tk.Tk):
         self.matrix_display.config(text=str(self.matrix_data))
 
 if __name__ == "__main__":
-    app = MatrixToolApp()
-    app.mainloop()
+    root = tk.Tk()
+    input_frame = InputFrame(root)
+    root.mainloop()
